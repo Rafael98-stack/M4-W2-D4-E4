a741bc5d10f31ec967ba23cf5b135a17a19b53be
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-01-18 16:38:34 +0100

    Developing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Supplier<Integer> integerSupplier = () -> {
+      Random rndm = new Random();
+      return rndm.nextInt(1, 100);
+    };
+    Supplier<User> userSupplier = () -> {
+      Faker faker = new Faker(Locale.ITALY);
+      return new User(faker.lordOfTheRings().character(), faker.name().lastName(), integerSupplier.get(), faker.gameOfThrones().city());
+    };
+    List<User> users = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      users.add(userSupplier.get());
+      System.out.println("La lista di utenti ha " + users.size() + " elementi");
+    }
   }
 }
